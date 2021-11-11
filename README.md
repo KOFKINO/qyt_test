a46733aae8bb1e1d8a6fba92896b589f83d5824e
wqx <kopfkino@yeah.net>
2021-11-11 16:39:48 +0800

    commit 4

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# test
+# <h2>test</h2>
+fasdffda
--- python.py
+++ python.py
@@ -1,9 +1,7 @@
 import pymongo
 print('hello world wqx')
 print('hello world wqx')
-<<<<<<< HEAD
 print('hello world wqx')
-=======
 print('hello world wqx!!')
->>>>>>> 88bb524 (Initial commit)
+
 
