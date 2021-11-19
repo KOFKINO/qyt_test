745a0d7db43fea009933a1f4b81b034451920b04
wqx <kopfkino@yeah.net>
2021-11-19 18:39:57 +0800

    get sn2

--- sn.py
+++ sn.py
@@ -1,24 +1,28 @@
 import re
-
-
-with open(r'C:\Users\admin.MENGFANDE3-PC\Desktop\S6800C.TG-CC.JXF','r',encoding = 'utf-8') as f:
-    str1 = f.read()
-
-    #slot cpu sn
-    slot = re.findall('(Slot\s+\d+\s+CPU\s+\d+):\s+\w+\s+:\s+\S+\s+DEVICE_(SERIAL_NUMBER\s+:\s+\S+)',str1)
-    for i in range(len(slot)):
-        print(slot[i][0])
-        print(slot[i][1])
-    #fan sn
-    fan = re.findall('(FAN\s+\d+\s):\s+\w+\s+:\s+\S+\s+DEVICE_(SERIAL_NUMBER\s+:\s+\S+)', str1)
-    for i in range(len(fan)):
-        print(fan[i][0])
-        print(fan[i][1])
-
-
-    #接口sn
-    interface = re.findall('(\w+-?Gig\S+)\s+transceiver\s+manufacture\s+information:\s+Manu. (Serial Number :\s+\w+)',str1)
-
-    for i in range(len(interface)):
-        print(interface[i][0])
-        print(interface[i][1])
+import os
+#获取当前路径:
+print (os.getcwd())
+# print(path)
+# with open(r'C:\Users\admin.MENGFANDE3-PC\Desktop\S6800C.TG-CC.JXF','r',encoding = 'utf-8') as f:
+#     str1 = f.read()
+#
+#     #slot cpu sn
+#     slot = re.findall('(Slot\s+\d+\s+CPU\s+\d+):\s+\w+\s+:\s+\S+\s+DEVICE_(SERIAL_NUMBER\s+:\s+\S+)',str1)
+#     for i in range(len(slot)):
+#         print(slot[i][0])
+#         print(slot[i][1])
+#     #fan sn
+#     fan = re.findall('(FAN\s+\d+\s):\s+\w+\s+:\s+\S+\s+DEVICE_(SERIAL_NUMBER\s+:\s+\S+)', str1)
+#     for i in range(len(fan)):
+#         print(fan[i][0])
+#         print(fan[i][1])
+#
+#
+#     #接口sn
+#     interface = re.findall('(\w+-?Gig\S+)\s+transceiver\s+manufacture\s+information:\s+Manu. (Serial Number :\s+\w+)',str1)
+#
+#     for i in range(len(interface)):
+#         print(interface[i][0])
+#         print(interface[i][1])
+# with open(r'C:\Users\admin.MENGFANDE3-PC\Desktop\sn.txt','w',encoding = 'utf-8') as f2:
+#
