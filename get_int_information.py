3b285f6ecfeb06a9cd4ee19f057a71ffff2c009e
wqx <kopfkino@yeah.net>
2021-11-25 15:04:57 +0800

    get int_infro

--- get_int_information.py
+++ get_int_information.py
@@ -1,11 +1,12 @@
 import re
+import kamene
 from ping_test import qytang_ping
 from ssh import qytang_ssh
 
 
 def get_int_infor(*ips, username='admin', password='H3c.com!123'):
-    device_int_dict = {}
-    qytang_ping(ips)
+    # device_int_dict = {}
+    print(qytang_ping(ips))
     # if qytang_ping(ips):
     #     infor_all = qytang_ssh(ips, username, password, cmd='dis ip int brief')
     #     for x in infor_all.split('\n'):
@@ -14,5 +15,5 @@ def get_int_infor(*ips, username='admin', password='H3c.com!123'):
 
 
 if __name__ == '__main__':
-    get_int_infor('172.16.1.3', username='admin', password='H3c.com!123')
+    get_int_infor('172.17.1.3', username='admin', password='H3c.com!123')
 
--- ping_test.py
+++ ping_test.py
@@ -6,10 +6,17 @@ from kamene.all import *
 def qytang_ping(ip):
     ping_pkt = IP(dst=ip) / ICMP()  # 制造一个Ping包
     ping_result = sr1(ping_pkt, timeout=2, verbose=False)  # Ping并且把返回结果复制给ping_result
+
     if ping_result:
-        return ip, 1
+        return ip
     else:
-        return ip, 0
+        return
 
 
-
+if __name__ == '__main__':
+    result = qytang_ping('172.17.1.3')
+    if result:
+        print(result + '通！')
+        print(type(result))
+    else:
+        print(result+ '不通！')
